64ef9c9bc752c082f2a764965654ac075d24f383
ikram-maulana <ikram075@ummi.ac.id>
2021-12-10 06:01:15 +0700

    Separate api for covid cases statistic

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,6 +1,7 @@
 import CONFIG from './config';
 
 const API_ENDPOINT = {
+  STATISTIC: `${CONFIG.STATISTIC_URL}api/indonesia`,
   MEDIA: `${CONFIG.MEDIA_URL}blogs`,
   VLOG: `${CONFIG.MEDIA_URL}vlog`,
   BlOGDETAIL: (id) => `${CONFIG.MEDIA_URL}blogs/${id}`,
--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
+  STATISTIC_URL: 'https://apicovid19indonesia-v2.vercel.app/',
   MEDIA_URL: 'https://61b090523c954f001722a461.mockapi.io/',
 };
 
