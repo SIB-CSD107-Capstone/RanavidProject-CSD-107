bab9c704d7db9adf04aa642688659f41ab9ae8ed
ikram-maulana <ikram075@ummi.ac.id>
2021-12-11 22:45:49 +0700

    Add confition if in production mode

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -7,6 +7,10 @@ export default function swRegister() {
     const wb = new Workbox('sw.js');
 
     wb.addEventListener('install', (event) => {
+      if (process.env.NODE_ENV === 'production') {
+        return;
+      }
+
       if (event.isUpdate) {
         console.log('Service worker not supported in this browser');
       }
