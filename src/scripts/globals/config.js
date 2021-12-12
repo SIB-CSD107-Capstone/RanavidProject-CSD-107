b105231b25011114d5f454fce80cc3a5bc8027f6
Febrianto100 <febrianto18@mhs.ubharajaya.ac.id>
2021-12-12 20:29:06 +0700

    install package idb

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,7 @@ const CONFIG = {
   STATISTIC_URL: 'https://apicovid19indonesia-v2.vercel.app/',
   RSBED_URL: 'https://rs-bed-covid-api.vercel.app/',
   MEDIA_URL: 'https://61b090523c954f001722a461.mockapi.io/',
+  IDB_NAME: 'bed-hospital-database',
 };
 
 export default CONFIG;
