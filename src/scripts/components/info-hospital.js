af3d1b93121592c9a92b3cda4a95128dcf7d96b2
Febrianto100 <febrianto18@mhs.ubharajaya.ac.id>
2021-12-15 16:45:29 +0700

    add logic if hotline equal null
    hotline yang ada di halaman detail rawat inap

--- src/scripts/components/info-hospital.js
+++ src/scripts/components/info-hospital.js
@@ -22,14 +22,14 @@ class InfoHospital extends HTMLElement {
         </button>
 
       </div>
-
+      
       <p class="rs-address" tabindex="0">${this._hospital.address}</p>
       <div class="detail-util d-flex flex-column flex-md-row">
         <a href="${this._hospital.gmaps}" target="_blank" rel="noopener" class="btn btn-outline-info btn-map pt-2 active-shadow mb-3 mb-md-0 mr-0 mr-md-3">Lihat Peta <span
             class="iconify" data-icon="simple-icons:googlemaps"></span></a>
-        <button class="btn btn-info btn-telp active-shadow" onclick="location.href = 'tel:+${this._hospital.phone}'"><span
+        <button class="btn btn-info btn-telp active-shadow" ${(this._hospital.phone) ? `onclick="location.href = tel:+${this._hospital.phone}` : ''}"><span
             class="telp-icon  iconify" data-icon="clarity:phone-handset-solid"></span>
-          <span class="no-telp">${this._hospital.phone}</span></button>
+          <span class="no-telp">${(this._hospital.phone) ? this._hospital.phone : 'hotline tidak tersedia'}</span></button>
       </div>
       `;
 
