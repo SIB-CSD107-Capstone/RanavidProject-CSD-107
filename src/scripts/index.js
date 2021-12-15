5bee5ea14609a6cecee739b45d4335e8b7d335be
Febrianto100 <febrianto18@mhs.ubharajaya.ac.id>
2021-12-15 19:12:02 +0700

    fix bug in btn hostline
    ngga jadi install aos via npm tetapi lewat cdn

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,10 +10,7 @@ import './components/loading-animation';
 import Swal from 'sweetalert2';
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import AOS from 'aos';
 import App from './views/app';
-
-AOS.init();
 
 const skipBtn = document.querySelector('skip-to-content');
 
